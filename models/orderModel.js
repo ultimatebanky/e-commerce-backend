a51c939ff5f3c5927fbcd50a1ac0a0e88a94afae
USER <user@USERs-MacBook-Pro.local>
2023-10-06 11:58:31 +0100

    last commit

--- models/orderModel.js
+++ models/orderModel.js
@@ -13,7 +13,6 @@ const orderSchema = mongoose.Schema(
     email: {
       type: String,
       required: [true, "Please add an email"],
-      unique: true,
       trim: true,
       match: [
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
